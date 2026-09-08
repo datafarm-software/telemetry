a075c646617c0e53362a87a3dd011bd20ad1cb46
Geraud <geraud2201@gmail.com>
2026-09-08 15:44:23 +0200

    use metering SetupFuncs

--- metering/metering.go
+++ metering/metering.go
@@ -5,8 +5,11 @@ import (
 	"time"
 )
 
+type SetupFunc func(name string) error
+
 type Meter interface {
 	Close(context.Context) error
+	Setup(name string, _ ...SetupFunc) error
 	RecordLatency(ctx context.Context, dur time.Duration) error
 	ActiveUsersCountAdd(i int)
 	CountApiRequest(ctx context.Context, i int, attr map[string]string)
@@ -14,7 +17,12 @@ type Meter interface {
 
 type MockMeter struct{}
 
-func (m *MockMeter) Close(context.Context) error                                        { return nil }
-func (m *MockMeter) RecordLatency(ctx context.Context, dur time.Duration) error         { return nil }
-func (m *MockMeter) ActiveUsersCountAdd(i int)                                          {}
+func (m *MockMeter) Close(context.Context) error { return nil }
+
+func (m *MockMeter) Setup() error { return nil }
+
+func (m *MockMeter) RecordLatency(ctx context.Context, dur time.Duration) error { return nil }
+
+func (m *MockMeter) ActiveUsersCountAdd(i int) {}
+
 func (m *MockMeter) CountApiRequest(ctx context.Context, i int, attr map[string]string) {}
